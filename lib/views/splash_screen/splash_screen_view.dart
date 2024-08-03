c466f2dff051497072b9e14d4c98b3c001792431
zuwhi <zuwhyyy@gmail.com>
2024-08-03 08:06:11 +0700

    add feature scanner

---
+++ lib/views/splash_screen/splash_screen_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: const Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
