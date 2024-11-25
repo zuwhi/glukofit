0bb6eda8ee3afa4831cf5314794f5b547a57954b
zuwhi <zuwhyyy@gmail.com>
2024-11-25 23:57:38 +0700

    change /predict and change com.example

--- lib/constants/app_constant.dart
+++ lib/constants/app_constant.dart
@@ -1,4 +1,4 @@
 class AppConstant {
-  static String urlDiagnose = "http://51.20.8.200";
+  static String urlDiagnose = "https://glukofit-flask-production.up.railway.app";
   static String urlGetProdukFromScrape = "https://scrap-gizi.vercel.app";
 }
